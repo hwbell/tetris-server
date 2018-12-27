e766e8c2a7672f16561d5519f417e47ab85830f4
Harry <harold.william.bell@gmail.com>
2018-12-27 14:12:35 -0700

    need to log the collection to see whats going on in heroku...

--- routes/scores.js
+++ routes/scores.js
@@ -10,6 +10,7 @@ let dbCollection = process.env.MONGODB_URI ? 'heroku_d19d3z3z' : 'tetris-app';
 router.get('/', function(req, res, next) {
   
   console.log('GET request for high scores received.');
+  console.log(`dbCollection: ${dbCollection}`)
 
   currentData = db.getScores('scores', dbCollection, (data) => {
     console.log(`Last score uploaded: ${data[data.length - 1]}`)
