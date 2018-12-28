5b04afaf5bd617b09c8f89a5fdfdcd24613e7612
Harry <harold.william.bell@gmail.com>
2018-12-28 12:05:08 -0700

    changed where cors code was, moved to top. hopfully this works

--- app.js
+++ app.js
@@ -4,28 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var scoresRouter = require('./routes/scores');
-
 var app = express();
 var port = process.env.PORT || 8080;
 var cors = require('cors');
+
+// for cors pre-flight requests
+app.options('*', cors());
+app.use(cors()); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// other tools
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var indexRouter = require('./routes/index');
+var scoresRouter = require('./routes/scores');
 app.use('/', indexRouter);
 app.use('/scores', scoresRouter);
-
-app.use(cors()); 
-app.options('*', cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
