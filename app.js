5fae4c92cdf8fb1fce41374e54593fe2a8e5a1ab
Harry <harold.william.bell@gmail.com>
2018-12-28 12:54:06 -0700

    added body parser back in place of express.json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var app = express();
@@ -18,8 +19,8 @@ app.set('view engine', 'pug');
 
 // other tools
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
